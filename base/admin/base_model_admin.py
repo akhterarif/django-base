60690e103fbfbea02f54673ebd3c4eec7397c3a4
Md. Akhter-uz-zaman Arif <arif.cse28@gmail.com>
2018-03-03 00:01:01 +0600

    Updated the files names from simple to model.

---
+++ base/admin/base_model_admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin, messages
+from django.utils.translation import ugettext_lazy as _
+
+
+class BaseModelAdmin(admin.ModelAdmin):
+    """
+    This class is base admin class. Whish holds the methods
+    and properties for common operations for admin.
+    """
+    list_display = ('id',
+                    'uuid',)
+    readonly_fields = ('date_created',
+                       'date_updated',
+                       'uuid',
+                       'id',
+                       'created_by',
+                       'updated_by',
+                       'deleted_by',)
+    search_fields = ('id',
+                     'uuid',)
+
+    def save_model(self, request, obj, form, change):
+        if obj.id:
+            obj.updated_by = request.user
+        else:
+            obj.created_by = request.user
+        obj.save()
