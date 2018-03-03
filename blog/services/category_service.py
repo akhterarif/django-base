2355a7978fa7aaf81eb2328e8d4293776d53f66d
Md. Akhter-uz-zaman Arif <arif.cse28@gmail.com>
2018-03-03 22:24:55 +0600

    Added base-service in category-service class.

--- blog/services/category_service.py
+++ blog/services/category_service.py
@@ -2,13 +2,12 @@ from ..models import CategoryModel
 from base.services import BaseService, BaseModelService
 
 
-class CategoryService(BaseService, BaseModelService):
+class CategoryService(BaseModelService, BaseService):
     """
     Category Service functions are enlisted here.
     """
 
     model = CategoryModel
 
-    def __init__(self, user):
-        self.user = user
-        super(CategoryService, self).__init__(user=self.user, model=self.model)
+    def __init__(self):
+        super(CategoryService, self).__init__()
