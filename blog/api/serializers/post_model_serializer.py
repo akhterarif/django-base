1d818806e1e44b7d46625d9add99b7ce04cc0faf
Md. Akhter-uz-zaman Arif <arif.cse28@gmail.com>
2018-03-04 00:32:15 +0600

    Added category response in post-model-serilaizer.

--- blog/api/serializers/post_model_serializer.py
+++ blog/api/serializers/post_model_serializer.py
@@ -1,10 +1,12 @@
 from base.api.serializers import BaseModelSerializer
 from ...models import PostModel
+from ..serializers import CategoryModelSerializer
 
 class PostModelSerializer(BaseModelSerializer):
 	"""
 	PostModelSerializer for post
 	"""
+	category = CategoryModelSerializer(read_only=True)
 
 	class Meta:
 		model = PostModel
