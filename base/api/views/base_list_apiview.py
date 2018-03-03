a3498a7447f3ec79dd0399cd871389eaa55fc26c
Md. Akhter-uz-zaman Arif <arif.cse28@gmail.com>
2018-03-03 21:35:27 +0600

    Added service_class property and get_queryset method in base_list_apiview.

--- base/api/views/base_list_apiview.py
+++ base/api/views/base_list_apiview.py
@@ -1,8 +1,20 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.mixins import ListModelMixin
 
-class BaseListAPIView(ListAPIView):
+class BaseListAPIView(ListAPIView, ListModelMixin):
 	"""
 	BaseListAPIView for the project
 	"""
+	service_class = None
+
 	def __init__(self):
 		super(BaseListAPIView, self).__init__()
+
+		assert self.service_class is not None, (
+				"'%s' must include a 'service' attribute."
+				% self.__class__.__name__
+		)
+
+	def get_queryset(self, **kwargs):
+		model_qs = self.service_class().list(**kwargs)
+		return model_qs
