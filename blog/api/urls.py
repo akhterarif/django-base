02cae93c2b988069f5424d0a7abdb92302daf7d6
Md. Akhter-uz-zaman Arif <arif.cse28@gmail.com>
2018-03-03 23:54:20 +0600

    Added post-list-apiview in urls.

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
-from blog.api.views import CategoryListAPIView
+from blog.api.views import CategoryListAPIView, Pos
 
 urlpatterns = [
     re_path(r'^category/$', CategoryListAPIView().as_view(), name='category'),
+    re_path(r'^post/$', CategoryListAPIView().as_view(), name='post'),
 ]
