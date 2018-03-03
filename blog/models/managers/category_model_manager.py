efb1ee37f68ecc9e7e06718e146356bf9fa59b45
Md. Akhter-uz-zaman Arif <arif.cse28@gmail.com>
2018-03-03 22:33:14 +0600

    Updated the category-model-magers initialization.

--- blog/models/managers/category_model_manager.py
+++ blog/models/managers/category_model_manager.py
@@ -2,5 +2,8 @@ from base.models.managers import BaseModelManager
 
 class CategoryModelManager(BaseModelManager):
 
-    def __init__(self, *args, **kwargs):
-        super(CategoryModelManager, self).__init__(*args, **kwargs)
+    def __init__(self,
+                 *args,
+                 **kwargs):
+        super(CategoryModelManager, self).__init__(*args,
+                                                   **kwargs)
