b8cd42176a33066b9196f9905ee27c7d8f4355b0
Md. Akhter-uz-zaman Arif <arif.cse28@gmail.com>
2018-03-04 01:00:49 +0600

    Added category_create api in urls.

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
-from blog.api.views import CategoryListAPIView, PostListAPIView
+from blog.api.views import CategoryListAPIView, CategoryCreateAPIView, PostListAPIView
 
 urlpatterns = [
+    re_path(r'^category/create/$', CategoryCreateAPIView().as_view(), name='category_create'),
     re_path(r'^category/$', CategoryListAPIView().as_view(), name='category'),
     re_path(r'^post/$', PostListAPIView().as_view(), name='post'),
 ]
