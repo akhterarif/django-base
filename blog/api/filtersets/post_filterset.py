588e66df73631c123e207c33ca2d152e18640ae1
Md. Akhter-uz-zaman Arif <arif.cse28@gmail.com>
2018-03-04 00:32:43 +0600

    Added category-filtering in posts-list api.

--- blog/api/filtersets/post_filterset.py
+++ blog/api/filtersets/post_filterset.py
@@ -5,7 +5,8 @@ from ...models import PostModel
 class PostFilterSet(BaseFilterSet):
 	title = CharFilter(name="title", lookup_expr='icontains')
 	text = CharFilter(name="text", lookup_expr='icontains')
+	category__name = CharFilter(name="category__name", lookup_expr='icontains')
 
 	class Meta:
 		model = PostModel
-		fields = ['category', 'title', 'text']
+		fields = ['category__name', 'title', 'text']
