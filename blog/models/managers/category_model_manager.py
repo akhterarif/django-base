0654272e87553505b30b0090210c23a24b96cb06
Md. Akhter-uz-zaman Arif <arif.cse28@gmail.com>
2018-03-03 17:57:13 +0600

    Added category-model-manager in category-model.

---
+++ blog/models/managers/category_model_manager.py
@@ -0,0 +1,6 @@
+from base.models.managers import BaseModelManager
+
+class CategoryModelManager(BaseModelManager):
+
+    def __init__(self, *args, **kwargs):
+        super(CategoryModelManager, self).__init__(*args, **kwargs)
