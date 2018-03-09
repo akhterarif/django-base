4422a02bb028da46c1eec027cd2fb7eb8f9f4d5a
Md. Akhter-uz-zaman Arif <arif.cse28@gmail.com>
2018-03-10 01:48:37 +0600

    Fixed base-queryset's datetime issue.

--- base/models/query_sets/base_queryset.py
+++ base/models/query_sets/base_queryset.py
@@ -1,12 +1,12 @@
 from django.db.models.query import QuerySet
-from datetime import timezone
+from datetime import datetime
 
 class BaseQuerySet(QuerySet):
     """
     BaseQuerySet for the project contains the base queryset related properties and methods.
     """
     def delete(self):
-        return super(BaseQuerySet, self).update(deleted_at=timezone.now())
+        return super(BaseQuerySet, self).update(deleted_at=datetime.now())
 
     def hard_delete(self):
         return super(BaseQuerySet, self).delete()
