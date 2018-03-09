38232e3ed8cabdbe7f31e0a6da1e111840cdd021
Md. Akhter-uz-zaman Arif <arif.cse28@gmail.com>
2018-03-09 16:35:23 +0600

    Added category-apiview.

--- blog/api/views/__init__.py
+++ blog/api/views/__init__.py
@@ -1,4 +1,5 @@
 from .category_list_apiview import CategoryListAPIView
 from .post_list_apiview import PostListAPIView
 from .category_create_apiview import CategoryCreateAPIView
+from .category_update_apiview import CategoryUpdateAPIView
 from .post_create_apiview import PostCreateAPIView
