04e9a85f2d0a2a35b2a138e926cb7c6e2b04693c
Md. Akhter-uz-zaman Arif <arif.cse28@gmail.com>
2018-03-09 16:35:45 +0600

    Added update api-url for category-update.

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import re_path, path
-from blog.api.views import CategoryListAPIView, CategoryCreateAPIView, PostListAPIView, PostCreateAPIView
+from blog.api.views import CategoryListAPIView, CategoryCreateAPIView, CategoryUpdateAPIView, PostListAPIView, PostCreateAPIView
 
 urlpatterns = [
 	path(r'category/create/', CategoryCreateAPIView().as_view(), name='category_create'),
+	path(r'category/update/<uuid:uuid>/', CategoryUpdateAPIView().as_view(), name='category_update'),
 	path(r'category/', CategoryListAPIView().as_view(), name='category'),
     path('post/create/', PostCreateAPIView().as_view(), name='post_create'),
     path(r'post/', PostListAPIView().as_view(), name='post'),
