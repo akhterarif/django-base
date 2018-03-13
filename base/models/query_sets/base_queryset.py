ca51ca93031b074903d20cefb29a9b76adcc8ece
Md. Akhter-uz-zaman Arif <arif.cse28@gmail.com>
2018-03-13 07:08:04 +0600

    Fixed the timezone issue in base-queryset.

--- base/models/query_sets/base_queryset.py
+++ base/models/query_sets/base_queryset.py
@@ -1,12 +1,12 @@
 from django.db.models.query import QuerySet
-from datetime import datetime
+from django.utils import timezone
 
 class BaseQuerySet(QuerySet):
     """
     BaseQuerySet for the project contains the base queryset related properties and methods.
     """
     def delete(self):
-        return super(BaseQuerySet, self).update(deleted_at=datetime.now())
+        return super(BaseQuerySet, self).update(deleted_at=timezone.now())
 
     def hard_delete(self):
         return super(BaseQuerySet, self).delete()
