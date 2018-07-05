5446607347b0c1e18b7a561bb5428662347713b7
Md. Akhter-uz-zaman Arif <arif.cse28@gmail.com>
2018-07-05 21:30:40 +0600

    Blog error has been fixed.

--- django_base/urls.py
+++ django_base/urls.py
@@ -4,11 +4,10 @@ from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify
 from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
-	path('docs/', include_docs_urls(title="Base app's Doc")),
+    path('docs/', include_docs_urls(title="Base app's Doc")),
     path('admin/', admin.site.urls),
     path('api-auth-token/', obtain_jwt_token),
     path('api-token-refresh/', refresh_jwt_token),
     path('api-token-verify/', verify_jwt_token),
     path('api/auth/', include('rest_framework.urls')),
-    path('api/blog/', include('blog.api.urls'), name='blog')
 ]
